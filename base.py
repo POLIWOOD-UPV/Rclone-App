ec64445a66804855a9d12e74aa9a439867e1bcef
Crazymango69 <theblazesev@gmail.com>
2026-05-10 13:14:56 +0200

    Crear paginas principales y botones que cambian de pantalla

--- base.py
+++ base.py
@@ -1,11 +1,24 @@
 from guizero import *
 
-def abrir_config():
-    Aplicacion.hide()
-    Config_w.show()
+def cambio_ventana(ventana_abrir,ventana_cerrar):
+    ventana_cerrar.hide()
+    ventana_abrir.show()
+
+def cierre_programa():
+    Aplicacion.after(1500,Aplicacion.destroy)
 
 Aplicacion= App(title="Rclone visual")
-Config_btt=PushButton(Aplicacion,command=abrir_config)
+B_Config=PushButton(Aplicacion,text="Config",command=lambda:cambio_ventana(W_Config,Aplicacion),width=15)
+B_Copy_Move=PushButton(Aplicacion,text="Copy/Move",command=lambda:cambio_ventana(W_Copy_Move,Aplicacion),width=15)
+B_cierre=PushButton(Aplicacion,text="Cerrar programa",align="bottom",command=cierre_programa)
 
-Config_w=Window(Aplicacion,visible=False)
 
+W_Config=Window(Aplicacion,visible=False)
+B_Volver_cnf=PushButton(W_Config,text="Inicio", command=lambda: cambio_ventana(Aplicacion,W_Config),align="bottom")
+
+W_Copy_Move=Window(Aplicacion,title="Copy/Move",visible=False)
+B_Volver_CM=PushButton(W_Copy_Move,text="Inicio",command=lambda: cambio_ventana(Aplicacion,W_Copy_Move),align="bottom")
+
+
+
+Aplicacion.display()
