58b2ea7e60b430b48386d4373437c3ecac3905b5
Charly <carlesfm0111@gmail.com>
2026-05-10 12:28:23 +0200

    Se ha hecho el config y se ha modulado el programa

--- subprocess.py
+++ subprocess.py
@@ -4,15 +4,65 @@ import subprocess
 if shutil.which("rclone") is None:
     print("rclone no está instalado o no está en PATH")
 
-directorios = subprocess.run(["rclone", "listremotes"], capture_output=True, text=True)
+opciones: str = input("¿Qué quieres hacer? (config/copy-move): ")
 
-cmd = input("Que quieres hacer (copy/move): ")
-dir1 = input("dir 1: ")
-dir2 = input("dir2: ")
-result = subprocess.run(["rclone", f"{cmd}", f"{dir1}", f"{dir2}", "--progress", "--ignore-existing"], capture_output=True, text=True)
+def copy_move() -> str:
+    cmd = input("Que quieres hacer (copy/move): ")
+    dir1 = input("dir 1: ")
+    dir2 = input("dir2: ")
+    result = subprocess.run(["rclone", f"{cmd}", f"{dir1}", f"{dir2}", "--progress", "--ignore-existing"], capture_output=True, text=True)
+    return result
 
-print(result.stdout) #Captura de salida
-print(result.stderr) #Captura de errores
+def buscar(salida) -> int:
+    opciones = {}
+    matches = re.findall(r'(\d+)\s+/\s+(.+)', salida)
+    for numero, nombre in matches:
+        opciones[nombre.strip()] = numero
+
+    for nombre, numero in opciones.items():
+        print(f"{nombre} -> {numero}\n")
+        
+    eleccion = input("Cual quieres: ")
+    return opciones[eleccion]
+
+def config():
+    nombre = input("Nombre del repositorio: ")
+    numero = 42
+    url = input("Direccion del sharepoint: ")
+    comando = subprocess.Popen(['rclone','config'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE, text=True)
+    salida,errores = comando.communicate(input=f"n\n{nombre}\n{numero}\n\n\n\n\n\n\n3\n{url}\n2\n\n\n")
+    #El 2 depende del sharepoint, 1 si es uno normal y 2 si es una personalizado
+    
+    print(salida)
+    
+    '''
+    comando.stdin.write("n\n")
+    comando.stdin.flush()
+    
+    comando.stdin.write(f"{nombre}\n")
+    comando.stdin.flush()
+    
+    comando.stdin.write(f"{numero}\n\n\n\n\n\n\n")
+    comando.stdin.flush()
+    
+    comando.stdin.write(f"3\n")
+    comando.stdin.flush()
+
+    https://upvedues.sharepoint.com/sites/HastaAcabarEstoCopias
+    '''
+
+if opciones == "config":
+    config()
+elif opciones == "copy-move":
+    result = copy_move()
+    print(result.stout)
+
+else:
+    print("La opción no es correcta")
+
+
+'''print(result.stdout) #Captura de salida
+print(result.stderr) #Captura de errores'''
 
 '''
 if result.returncode == 0: #Si no da error
