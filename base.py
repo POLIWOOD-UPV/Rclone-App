869c831b52c5260a85c051a9b3bcdf3e9939893f
Charly <carlesfm0111@gmail.com>
2026-05-31 10:52:34 +0200

    Refactor button configuration in the settings window

--- base.py
+++ base.py
@@ -32,7 +32,7 @@ T_nombre_repo=Text(Config_box_izq,text="Nombre del repositorio",grid=[0,0],align
 Tb_nombre_repo=TextBox(Config_box_izq,grid=[1,0],align="left",width=30)
 T_url=Text(Config_box_izq,text="Direccion del sharepoint",grid=[0,1],align="left")
 Tb_url=TextBox(Config_box_izq,grid=[1,1],align="left",width=30)
-B_anadir_repo=PushButton(Bx_Config, text="Añadir repos.", command=lambda:config(Tb_nombre_repo.value, Tb_url.value))
+B_anadir_repo=PushButton(Config_box_izq, text="Añadir repos.", grid=[0,2], command=lambda:config(Tb_nombre_repo.value, Tb_url.value))
 T_prueba=Text(Config_box_der,text="Texto de prueba",grid=[1,2],align="left")
 B_volvel_config=PushButton(W_Config,text="Inicio",align="bottom",command=lambda: cambio_ventana(Aplicacion,W_Config))
 
